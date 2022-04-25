cd4d17da90f7955d84c0f1292350891388db5197
dhruv035 <dhruv035@gmail.com>
2022-04-25 09:05:19 +0530

    Fixes and Indentation

--- contracts/AProxy.sol
+++ contracts/AProxy.sol
@@ -12,24 +12,21 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract AProxy is Ownable, ERC2771Context{
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
+address public AAVE_LENDING_POOL_ADDRESS = 0x3561c45840e2681495ACCa3c50Ef4dAe330c94F8;
+Pool public lendingPool = Pool(AAVE_LENDING_POOL_ADDRESS);
 
-  constructor(address _trustedForwarder) ERC2771Context(_trustedForwarder){}
+constructor(address _trustedForwarder) ERC2771Context(_trustedForwarder){}
 
 function _msgSender() internal view override(Context, ERC2771Context) returns (address){
+
         return ERC2771Context._msgSender();
     }
 function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata){
 
         return ERC2771Context._msgData();
-
     }
-address public AAVE_LENDING_POOL_ADDRESS = 0xE039BdF1d874d27338e09B55CB09879Dedca52D8;
-Pool public lendingPool = Pool(AAVE_LENDING_POOL_ADDRESS);
-
-function depositToAave(address asset, uint256 amount,uint16 referralCode) public  {
-
+function depositToAave(address asset, uint256 amount,uint16 referralCode) external {
         IERC20 underlying = IERC20(asset);
-
         underlying.transferFrom(_msgSender(), address(this), amount);
         underlying.approve(AAVE_LENDING_POOL_ADDRESS, amount);
         lendingPool.supply(asset, amount,_msgSender(), referralCode);
